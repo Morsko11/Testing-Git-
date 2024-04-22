5dbd3cc7adbb3c781816ebc7eb31b447b34ada41
Morsko11 <morsko11@mail.ru>
2024-04-22 16:08:58 +0700

    test1 create

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
-    void test(){
+
+    void test() {
         System.out.println("Test");
     }
-    void help(){
-        int x;
-        System.out.println("help");
-
+    void test1() {
+        System.out.println("Test");
     }
 }
+
+
+
